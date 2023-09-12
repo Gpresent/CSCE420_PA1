58c5e69c4b5f87f4f3e5b73b5689011749625f8c
Cole Broberg <gpresent@tamu.edu>
2023-09-12 18:30:31 -0500

    Altered algorithm to use hash table to store known states

--- blocksworld.py
+++ blocksworld.py
@@ -1,24 +1,66 @@
 import sys
+import heapq
 
 args = sys.argv[1:]
 filename = args[0]
 
-#generates all possible next states from current state
-def genstates(state):
+
+# definition of state class
+class state:
+    def __init__(self, cond, heuristic):
+        self.cond = cond
+        self.parent = None
+        self.heuristic = heuristic
+
+    def __lt__(self, other):
+        return self.heuristic < other.heuristic
+
+    def __eq__(self, other):
+        return self.cond == other.cond
+
+    def __str__(self):
+        return str(self.cond) + " " + str(self.heuristic)
+
+
+# grades a state and assigns heuristic value
+def gradestate(state):
+    return 1
+
+
+# function that adds state to the hash table of known states
+def addstate(state, table):
+    statestr = state.cond[0]
+    for i in state.cond[1:]:
+        statestr += "-" + i
+    table[statestr] = 1
+
+
+# function that checks whether state is in the hash table of known states
+def checkstate(state, table):
+    statestr = state.cond[0]
+    for i in state.cond[1:]:
+        statestr += "-" + i
+    return table.get(statestr) is not None
+
+
+# generates all possible states from current state
+def genstates(parent_state):
     states = []
-    for i in range(0, len(state)):
-        if state[i] == '':
+    for i in range(0, len(parent_state.cond)):
+        if parent_state.cond[i] == "":
             continue
-        for j in range(1, len(state)):
-            newstate=state.copy()
-            lastdig = newstate[i][-1]
-            newstate[i] = newstate[i][0:-1]
-            newstate[(i+j)%len(state)] += lastdig
+        for j in range(1, len(parent_state.cond)):
+            newcond = parent_state.cond.copy()
+            lastdig = newcond[i][-1]
+            newcond[i] = newcond[i][0:-1]
+            newcond[(i + j) % len(parent_state.cond)] += lastdig
+            newstate = state(newcond, gradestate(newcond))
+            newstate.parent = parent_state
             states.append(newstate)
     return states
 
-    
-with open("probs/" + filename, 'r') as f:
+
+with open("probs/" + filename, "r") as f:
     # read basic file info
     lines = f.readlines()
     col_num, block_num, move_num = lines[0].strip().split()
@@ -27,23 +69,40 @@ with open("probs/" + filename, 'r') as f:
     move_num = int(move_num)
 
     # read initial state and goal state from file
-    state = []
+    cond = []
     line_num = 2
     for i in range(0, col_num):
-        state.append(lines[line_num + i].strip())
+        cond.append(lines[line_num + i].strip())
     line_num += int(col_num) + 1
     goal = []
     for i in range(0, col_num):
         goal.append(lines[line_num + i].strip())
-    
+
     # debug printouts
-    print("state:", state)
+    print("state:", cond)
     print("goal:", goal)
-    states = genstates(state)
-
-
-    d = dict()
-    for s in states:
-        d[s] = 1
-    print("possible states:", states)
-
+    ancestor_state = state(cond, gradestate(cond))
+    states = genstates(ancestor_state)
+    known_states = dict()
+    addstate(ancestor_state, known_states)
+    heapq.heapify(states)
+    while len(states) > 0:
+        best = heapq.heappop(states)
+        if best.cond == goal:
+            print("goal reached\nFinal state:", best.cond)
+            pathtogoal = []
+            while best.parent is not None:
+                pathtogoal.append(best.cond)
+                best = best.parent
+            print("path to goal:")
+            print(ancestor_state.cond)
+            for i in pathtogoal[::-1]:
+                print(i)
+            print("number of moves:", len(pathtogoal))
+            break
+        else:
+            newstates = genstates(best)
+            for i in newstates:
+                if not checkstate(i, known_states):
+                    heapq.heappush(states, i)
+                    addstate(i, known_states)
